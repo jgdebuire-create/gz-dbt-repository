170eabc075466020ec8f441563bc8214d92780c7
jgdebuire@gmail.com <jgdebuire@gmail.com>
2026-02-05 07:56:32 +0000

    bug fix

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,16 +1,18 @@
 SELECT
-     o.orders_id
-     ,o.date_date
-     ,ROUND(o.margin + s.shipping_fee - (s.log_cost + s.ship_cost),2) AS operational_margin
-     ,o.quantity
-     ,o.revenue
-     ,o.purchase_cost
-     ,o.margin
-     ,s.shipping_fee
-     ,s.log_cost
-     ,s.ship_cost
- FROM {{ref("int_orders_margin")}} o
- LEFT JOIN {{ ref("stg_Raw__ship") }} s 
-
-     USING(orders_id)
- ORDER BY orders_id desc
+  o.orders_id,
+  o.date_date,
+  ROUND(
+    o.margin
+    + COALESCE(s.shipping_fee, 0)
+    - (COALESCE(s.logcost, 0) + COALESCE(s.ship_cost, 0))
+  , 2) AS operational_margin,
+  o.quantity,
+  o.revenue,
+  o.purchase_cost,
+  o.margin,
+  s.shipping_fee,
+  s.logcost,
+  s.ship_cost
+FROM {{ ref("int_orders_margin") }} o
+LEFT JOIN {{ ref("stg_Raw__ship") }} s
+  ON o.orders_id = s.orders_id
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,15 +1,14 @@
-
 {{ config(materialized='view') }}
 
-  SELECT
-      products_id,
-      date_date,
-      orders_id,
-      revenue,
-      quantity,
-      purchase_price,
-      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-FROM {{ ref('stg_Raw__sales') }} s
-LEFT JOIN {{ ref('stg_Raw__product') }} p
-      USING (products_id)
+SELECT
+  p.products_id,
+  s.date_date,
+  s.orders_id,
+  s.revenue,
+  s.quantity,
+  p.purchase_price,
+  ROUND(s.quantity * p.purchase_price, 2) AS purchase_cost,
+  ROUND(s.revenue - s.quantity * p.purchase_price, 2) AS margin
+FROM {{ ref("stg_Raw__sales") }} s
+LEFT JOIN {{ ref("stg_Raw__product") }} p
+  ON s.pdt_id = p.products_id
